cf6f86fcd5355311260895065b7a3a9f6835ff76
varun <varun@srtechops.com>
2026-05-13 03:55:10 +0530

    updated the sonarqube code

--- backend/src/test/java/com/portfolio/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/portfolio/backend/BackendApplicationTests.java
@@ -3,7 +3,16 @@ package com.portfolio.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.TestPropertySource;
+
 @SpringBootTest
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driverClassName=org.h2.Driver",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+})
 class BackendApplicationTests {
 
 	@Test
